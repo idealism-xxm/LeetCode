67f8328ae0c96c7cc778334f1b7913972f66bea0
idealism-xxm <biaochekuangmo@yeah.net>
2021-10-02 16:20:19 +0800

    docs: LeetCode 2008 (Python3)

--- Python3/src/2001 ~ 3000/2008 - Maximum Earnings From Taxi.py
+++ Python3/src/2001 ~ 3000/2008 - Maximum Earnings From Taxi.py
@@ -25,7 +25,7 @@
 #   总共能赚 9 + 5 + 6 = 20
 
 
-# 思路： DP
+# 思路1： DP
 #
 #       我们先用 map 映射每个起点的乘客列表 start_to_rides ，
 #       设 dp[i] 表示汽车走到第 i 个点时，能赚的最大钱数。
@@ -61,3 +61,62 @@ class Solution:
                 dp[end] = max(dp[end], dp[start] + end - start + tip)
         
         return dp[n]
+
+
+# 思路2： DP
+#
+#       看了讨论区后，发现了另一种思路，
+#       思路 1 是以距离为状态枚举，而这里是以乘客为状态枚举，
+#       这样在距离特别大的情况下也可以很快求出结果。
+#
+#       首先对 rides 按照起点升序排序，
+#       设 dp[i] 表示 rides[i:] 中能赚到的最大钱数，
+#
+#       初始化： dp[1~m] = 0
+#       状态转移：
+#           倒序枚举乘客，假设当前第 i 个乘客为 [start, end, tip] ，
+#           拉完 rides[i:] 中的某些乘客后能赚到的最大钱数有两种情况：
+#               1. 不拉当前乘客，则能赚 dp[i + 1] 元
+#               2. 拉当前乘客，走到 end 处，赚 end - start + tip 元，
+#                   然后从 end 开始继续拉乘客，赚取可能的最大的钱。
+#                   我们注意到 rides 是升序的，所以我们可以二分 rides[i:] 中的乘客，
+#                   找到第一个乘客的起点大于等于 end 的乘客下标 k ，
+#                   则后续可以赚到的最大钱数为 dp[k] ，
+#                   即总共赚到的钱数为 end - start + tip + dp[k]
+#           综上： dp[i] = max(dp[i + 1], end - start + tip + dp[k])
+#               
+#
+#       最终 dp[0] 就是出租车能赚的最大钱数
+#       
+#       时间复杂度： O(mlogm)
+#       空间复杂度： O(m)
+
+
+class Solution:
+    def maxTaxiEarnings(self, n: int, rides: List[List[int]]) -> int:
+        # 乘客列表按照起点升序排序
+        rides.sort()
+        
+        # 初始化 dp
+        m = len(rides)
+        dp = [0] * (m + 1)
+        # 状态转移，倒序枚举乘客
+        for i in range(m - 1, -1, -1):
+            start, end, tip = rides[i]
+            # 二分找到第一个起点大于等于终点的乘客
+            l, r = i + 1, m - 1
+            while l <= r:
+                mid = (l + r) // 2
+                if rides[mid][0] >= end:
+                    r = mid - 1
+                else:
+                    l = mid + 1
+            # 第 k 个乘客的起点是第一个大于等于 end 的乘客
+            k = l
+
+            # 1. 不拉当前乘客，则能赚 dp[i + 1] 元
+            # 2. 拉当前乘客，走到 end 处，能赚 end - start + tip 元，
+            #    能从剩余的 rides[k:] 中赚到 dp[k] 元
+            dp[i] = max(dp[i + 1], end - start + tip + dp[k])
+
+        return dp[0]
