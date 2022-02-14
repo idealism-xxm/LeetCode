c890b04972fd6935dcf2f8f8a0a2fa0662681ff6
idealism-xxm <biaochekuangmo@yeah.net>
2022-02-14 20:27:25 +0800

    docs: LeetCode 0104 (Rust)

--- Rust/src/0101 ~ 0200/0104 - Maximum Depth of Binary Tree.rs
+++ Rust/src/0101 ~ 0200/0104 - Maximum Depth of Binary Tree.rs
@@ -33,24 +33,37 @@
 //      时间复杂度：O(n) 。每一个结点都需要便利，所以时间复杂度是 O(n) 。
 //      空间复杂度：O(n) 。最差情况下，树的高度是 n ，需要开辟 n 层栈空间，所以空间复杂度是 O(n) 。
 
-/**
- * Definition for a binary tree node.
- * type TreeNode struct {
- *     Val int
- *     Left *TreeNode
- *     Right *TreeNode
- * }
- */
-func maxDepth(root *TreeNode) int {
-	if root == nil {
-		return 0
-	}
-	return 1 + max(maxDepth(root.Left), maxDepth(root.Right))
-}
+// Definition for a binary tree node.
+// #[derive(Debug, PartialEq, Eq)]
+// pub struct TreeNode {
+//   pub val: i32,
+//   pub left: Option<Rc<RefCell<TreeNode>>>,
+//   pub right: Option<Rc<RefCell<TreeNode>>>,
+// }
 
-func max(a, b int) int {
-	if a > b {
-		return a
-	}
-	return b
+// impl TreeNode {
+//   #[inline]
+//   pub fn new(val: i32) -> Self {
+//     TreeNode {
+//       val,
+//       left: None,
+//       right: None
+//     }
+//   }
+// }
+use std::rc::Rc;
+use std::cell::RefCell;
+impl Solution {
+    pub fn max_depth(root: Option<Rc<RefCell<TreeNode>>>) -> i32 {
+        if let Some(root) = root {
+            // 如果根结点存在，则返回左右子树的最大深度加 1
+            1 + i32::max(
+                Self::max_depth(root.borrow().left.clone()), 
+                Self::max_depth(root.borrow().right.clone()),
+            )
+        } else {
+            // 如果根结点不存在，则返回 0
+            0
+        }
+    }
 }
