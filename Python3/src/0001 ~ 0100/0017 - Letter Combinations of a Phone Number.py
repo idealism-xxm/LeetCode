a7e6eff796c3b0ee9aa54fd7b3f5eddbf1242e58
idealism-xxm <biaochekuangmo@yeah.net>
2022-05-10 23:04:22 +0800

    docs: LeetCode 0017 (Go|Python3|Rust)

--- Python3/src/0001 ~ 0100/0017 - Letter Combinations of a Phone Number.py
+++ Python3/src/0001 ~ 0100/0017 - Letter Combinations of a Phone Number.py
@@ -19,7 +19,7 @@
 # 输出： ["a","b","c"]
 
 
-# 思路： 递归
+# 思路1： 递归
 #
 #      我们使用 dfs(digits, index, cur, ans) 遍历收集所有可能的字符串，其中：
 #          1. digits: 输入的数字串
@@ -75,3 +75,65 @@ class Solution:
             cur[index] = ch
             # 递归收集下一个字母
             Solution.dfs(digits, index + 1, cur, ans)
+
+
+# 思路2： 迭代
+#
+#      由于每个数字都会且仅会产生一个字母，
+#      那么最终所有可能的字符串长度都是 len(digits) ，
+#      即字符串的第 i 个字母是由 digits[i] 决定的。
+#
+#      如果知道 digits[:i] 对应的一个可能的字符串 cur ，
+#      那么在 cur 后面分别加上 digits[i] 对应的字母 ch ，
+#      即可得到 digits[:i + 1] 对应的一些可能的字符串。
+#
+#      我们可以运用递推的思路使用迭代处理，
+#      让 ans[i] 表示 digits[:i] 对应的所有可能的字符串。
+#
+#      遍历 ans[i] 中的每个字符串 cur ，
+#      在其后面加上 digits[i] 对应的每个字母 ch 组成新的字符串，
+#      所有这样的新字符串就组成了 ans[i + 1] 。
+#
+#      初始化令 ans[0] = [""] ，
+#      那么最后 ans[len(digits)] 就是所有可能的字符串，
+#      实际处理时可以优化为滚动数组，降低空间复杂度。
+#
+#
+#      时间复杂度：O(4 ^ n)
+#          1. 需要遍历全部可能的字符串，最差情况下所有的数字键都是 4 个字母的，
+#              共有 O(4 ^ n) 个可能的字符串
+#      空间复杂度：O(4 ^ n)
+#          1. 需要收集全部可能的字符串，最差情况下所有的数字键都是 4 个字母的，
+#              共有 O(4 ^ n) 个可能的字符串
+
+
+# 定义每个数位对应的字母列表
+DIGIT_TO_LETTERS: List[str] = ["", "", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"];
+
+
+class Solution:
+    def letterCombinations(self, digits: str) -> List[str]:
+        # 如果没有按任何数字键，则返回空列表
+        if not digits:
+            return []
+
+        # ans 用于收集所有可能的字符串，
+        # 初始放入空串，方便后续迭代
+        ans: List[str] = [""]
+        # 遍历数字串的数位 digit
+        for digit in digits:
+            # 获取数位对应的字母列表
+            letters: str = DIGIT_TO_LETTERS[ord(digit) - ord('0')]
+            # 定义下一个可能的字符串列表，容量为 ans 的长度乘以 letters 的长度，
+            # 因为 ans 中的每个字符串都会被加上 letters 中的每个字母
+            nxt: List[str] = []
+            # 遍历 ans 中的每个字符串
+            for cur in ans:
+                # 遍历 letters 中的每个字母
+                for ch in letters:
+                    # 拼接 digit 对应的所有字母，然后放入 nxt 中
+                    nxt.append(cur + ch)
+
+            ans = nxt
+
+        return ans
--- Rust/src/0001 ~ 0100/0017 - Letter Combinations of a Phone Number.rs
+++ Rust/src/0001 ~ 0100/0017 - Letter Combinations of a Phone Number.rs
@@ -19,7 +19,7 @@
 // 输出： ["a","b","c"]
 
 
-// 思路： 递归
+// 思路1： 递归
 //
 //      我们使用 dfs(digits, index, cur, ans) 遍历收集所有可能的字符串，其中：
 //          1. digits: 输入的数字串
@@ -78,5 +78,73 @@ impl Solution {
             // 递归收集下一个字母
             Solution::dfs(digits, index + 1, cur, ans);
         }
+    }
+}
+
+
+// 思路2： 迭代
+//
+//      由于每个数字都会且仅会产生一个字母，
+//      那么最终所有可能的字符串长度都是 len(digits) ，
+//      即字符串的第 i 个字母是由 digits[i] 决定的。
+//
+//      如果知道 digits[:i] 对应的一个可能的字符串 cur ，
+//      那么在 cur 后面分别加上 digits[i] 对应的字母 ch ，
+//      即可得到 digits[:i + 1] 对应的一些可能的字符串。
+//
+//      我们可以运用递推的思路使用迭代处理，
+//      让 ans[i] 表示 digits[:i] 对应的所有可能的字符串。
+//
+//      遍历 ans[i] 中的每个字符串 cur ，
+//      在其后面加上 digits[i] 对应的每个字母 ch 组成新的字符串，
+//      所有这样的新字符串就组成了 ans[i + 1] 。
+//
+//      初始化令 ans[0] = [""] ，
+//      那么最后 ans[len(digits)] 就是所有可能的字符串，
+//      实际处理时可以优化为滚动数组，降低空间复杂度。
+//
+//
+//      时间复杂度：O(4 ^ n)
+//          1. 需要遍历全部可能的字符串，最差情况下所有的数字键都是 4 个字母的，
+//              共有 O(4 ^ n) 个可能的字符串
+//      空间复杂度：O(4 ^ n)
+//          1. 需要收集全部可能的字符串，最差情况下所有的数字键都是 4 个字母的，
+//              共有 O(4 ^ n) 个可能的字符串
+
+
+// 定义每个数位对应的字母列表
+const DIGIT_TO_LETTERS: [&str; 10] = ["", "", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"];
+
+
+impl Solution {
+    pub fn letter_combinations(digits: String) -> Vec<String> {
+        // 如果没有按任何数字键，则返回空列表
+        if digits.is_empty() {
+            return vec![];
+        }
+
+        // ans 用于收集所有可能的字符串，
+        // 初始放入空串，方便后续迭代
+        let mut ans = vec![String::new()];
+        // 遍历数字串的数位 digit
+        for &digit in digits.as_bytes() {
+            // 获取数位对应的字母列表
+            let letters = DIGIT_TO_LETTERS[(digit - b'0') as usize];
+            // 定义下一个可能的字符串列表，容量为 ans 的长度乘以 letters 的长度，
+            // 因为 ans 中的每个字符串都会被加上 letters 中的每个字母
+            let mut nxt = Vec::with_capacity(ans.len() * letters.len());
+            // 遍历 ans 中的每个字符串
+            for cur in ans {
+                // 遍历 letters 中的每个字母
+                for ch in letters.chars() {
+                    // 拼接 digit 对应的所有字母，然后放入 nxt 中
+                    nxt.push(cur.clone() + &ch.to_string());
+                }
+            }
+
+            ans = nxt;
+        }
+
+        ans
     }
 }
