49d239854e3b4589a8514fbb4dcecc39ca6b92e7
idealism-xxm <biaochekuangmo@yeah.net>
2022-08-16 13:34:18 +0800

    docs: LeetCode 0387 (Go|Python3|Rust)

---
+++ Rust/src/0301 ~ 0400/0387 - First Unique Character in a String.rs
@@ -0,0 +1,65 @@
+// 链接：https://leetcode.com/problems/first-unique-character-in-a-string/
+// 题意：给定一个字符串 s ，求其中第一个不重复的字符下标？如果不存在则返回 -1 。
+
+
+// 数据限制：
+//  1 <= s.length <= 10 ^ 5
+//  s 只包含英文小写字母
+
+
+// 输入： s = "leetcode"
+// 输出： 0
+// 解释： 字母 'l' 是第一个不重复的字符
+
+// 输入： s = "loveleetcode"
+// 输出： 2
+// 解释： 字母 'v' 是第一个不重复的字符
+
+// 输入： s = "aabb"
+// 输出： -1
+// 解释： 不存在不重复的字符
+
+
+// 思路： Map
+//
+//      我们直接用一个名为 ch_to_cnt 的 map 维护每个字符的出现次数。
+//
+//      先遍历一遍 s ，统计每个字符的出现次数。
+//
+//      然后再遍历一遍 s ，遇到第一个出现次数为 1 的字符，直接返回其下标即可。
+//
+//      如果最后还没有返回，则没有不重复的字符，返回 -1 。
+//
+//
+//      设 C 为字符集大小。
+//
+//      时间复杂度：O(n)
+//          1. 需要遍历 s 中全部 O(n) 个字符两次
+//      空间复杂度：O(1)
+//          1. 需要维护全部 O(C) 个不同字符的出现次数
+
+
+use std::collections::HashMap;
+use std::ops::AddAssign;
+
+impl Solution {
+    pub fn first_uniq_char(s: String) -> i32 {
+        let s = s.as_bytes();
+        // ch_to_cnt[ch] 表示 ch 出现的次数
+        let mut ch_to_cnt = HashMap::new();
+        // 统计 s 中每个字符的出现次数
+        for ch in s.iter() {
+            ch_to_cnt.entry(ch).or_insert(0).add_assign(1);
+        }
+
+        // 再遍历一遍 s，找到第一个出现次数为 1 的字符，并返回其下标
+        for (i, ch) in s.iter().enumerate() {
+            if ch_to_cnt[ch] == 1 {
+                return i as i32;
+            }
+        }
+
+        // 此时没有不重复的字符，返回 -1
+        -1
+    }
+}
