cfb9d2deaefe87187430a088973ab86891681136
idealism-xxm <biaochekuangmo@yeah.net>
2023-12-30 22:25:43 +0800

    docs: LeetCode 1974 (Go|Python3|Rust)

--- Python3/src/1901 ~ 2000/1974 - Minimum Time to Type Word Using Special Typewriter.py
+++ Python3/src/1901 ~ 2000/1974 - Minimum Time to Type Word Using Special Typewriter.py
@@ -1,11 +1,14 @@
 # 链接：https://leetcode.com/problems/minimum-time-to-type-word-using-special-typewriter/
 # 题意：给定一个刻着 26 个字母的圆盘，有一个指针初始指向字母 'a' ，
-#       每 1 秒可以讲指针移向相邻的字母，或者打出当前指针指向的字母。
-#       求打出给定单词 word 的最小时间？
+#      每 1 秒可以将指针移向相邻的字母，或者打出指针当前指向的字母。
+#
+#      求打出给定单词 word 的最小时间？
+
 
 # 数据限制：
 #   1 <= word.length <= 100
 #   word 均有英文小写字母组成
+
 
 # 输入： word = "abc"
 # 输出： 5
@@ -43,35 +46,43 @@
 #       第 34 秒：打出 'c'
 
 
-# 思路： 模拟
+# 思路： 贪心
 #
-#       模拟当前指针指向字母的下标 cur ，
-#       遍历下一个要打的字母的下标 idx ，
+#       模拟当前指针指向字母的下标 cur ，遍历下一个要打的字母的下标 idx 。
+#
 #       假设顺时针从 cur 移动至 idx ，则要消耗 cnt = idx - cur 秒，
-#       如果 cnt < 0 ，则进入了下一圈，需要加上 26
-#       那么逆时针从 cur 移动至 idx ，则要消耗 26 - cnt 秒，
-#       两者取较小值即可，
-#       那么打印出 idx 的字母耗时为 min(cnt, 26 - cnt) + 1 秒
-#       
-#       时间复杂度： O(|word|)
+#       如果 cnt < 0 ，则进入了下一圈，需要加上 26 。
+#
+#       那么逆时针从 cur 移动至 idx ，则要消耗 26 - cnt 秒，两者取较小值即可。
+#
+#       即打印出 idx 的字母耗时为 min(cnt, 26 - cnt) + 1 秒。
+#
+#
+#       时间复杂度： O(n)
+#          1. 需要遍历 word 中全部 O(n) 个字符
 #       空间复杂度： O(1)
+#          1. 只需要维护常数个额外变量
+
 
 class Solution:
     def minTimeToType(self, word: str) -> int:
-        a_ord = ord('a')
-        cur = 0
-        ans = 0
+        # ans 维护打完 word 中全部字符所需的时间
+        ans: int = 0
+        # cur 维护指针当前指向的字母下标
+        cur: int = 0
+        # 遍历 word 中每个单词
         for ch in word:
-            # 计算字母的下标
-            idx = ord(ch) - a_ord
+            # 计算该字母的下标
+            idx: int = ord(ch) - ord('a')
             
             # 计算顺时针移动所需的时间
-            cnt = idx - cur
+            cnt: int = idx - cur
             if cnt < 0:
                 cnt += 26
-            # 移动时间为顺时针和逆时针时间的较小值
+            # 移动时间为顺时针和逆时针时间的较小值，
             # 还要加上打印字母的时间
             ans += min(cnt, 26 - cnt) + 1
             # 指针移动至 idx 对应的字母
             cur = idx
+
         return ans
